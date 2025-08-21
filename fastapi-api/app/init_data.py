86631b84faff03ecb04dc758ff5f280a83a73814
Phai <bicwmeme@gmail.com>
2025-08-21 11:59:37 +0700

    Day 4.7

---
+++ fastapi-api/app/init_data.py
@@ -0,0 +1,14 @@
+import csv
+from .database import SessionLocal
+from . import crud
+
+def import_csv(filepath: str = "branches.csv"):
+    db = SessionLocal()
+    with open(filepath, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            crud.create_branch(db, name=row['branch_name'], ip=row['ip'])
+    db.close()
+
+if __name__ == "__main__":
+    import_csv()
