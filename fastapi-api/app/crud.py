6f715080c36575e71c6770fbcf82ea9a465c4d23
Phai <bicwmeme@gmail.com>
2025-09-05 16:15:49 +0700

    .

--- fastapi-api/app/crud.py
+++ fastapi-api/app/crud.py
@@ -4,8 +4,8 @@ from . import models, schemas
 def get_branches(db: Session):
     return db.query(models.Branch).all()
 
-def create_branch(db: Session, branch: schemas.BranchCreate):
-    db_branch = models.Branch(branch_name=branch.branch_name, ip=branch.ip)
+def create_branch(db: Session, name: str, ip: str):
+    db_branch = models.Branch(branch_name=name, ip=ip)
     db.add(db_branch)
     db.commit()
     db.refresh(db_branch)
