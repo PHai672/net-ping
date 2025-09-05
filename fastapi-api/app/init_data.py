4f12314e446590f06c1b91e6345d394b5241666b
Phai <bicwmeme@gmail.com>
2025-09-05 16:09:43 +0700

    .

--- fastapi-api/app/init_data.py
+++ fastapi-api/app/init_data.py
@@ -1,8 +1,9 @@
 import csv
+import os
 from .database import SessionLocal
 from . import crud, models, schemas
 
-def import_csv(filepath: str = "branches.csv"):
+def import_csv(filepath: str = os.path.join(os.path.dirname(__file__), "../branches.csv")):
     db = SessionLocal()
     with open(filepath, newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
